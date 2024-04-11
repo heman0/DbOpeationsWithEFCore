54aef46a1ea8aacebe81c2785428657fb5591d81
Heman0 <fmail9899@gmail.com>
2024-04-11 16:11:51 +0530

    Added Currency,Language Controllers and sucessfully run the API

---
+++ Controllers/CurrencyController.cs
@@ -0,0 +1,33 @@
+﻿using DbOpeationsWithEFCore.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbOpeationsWithEFCore.Controllers
+{
+    [Route("api/currencies")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public CurrencyController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCurrencies()
+        {
+            // var result= this.context.tbl_Currencies.ToList();
+            var result = await (from currencies in context.tbl_Currencies
+                               select currencies).ToListAsync();
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCurrencyByIdAsync(int id)
+        {
+            var result = await context.tbl_Currencies.FindAsync(id);
+            return Ok(result);
+        }
+    }
+}
---
+++ Controllers/LanguageController.cs
@@ -0,0 +1,33 @@
+﻿using DbOpeationsWithEFCore.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbOpeationsWithEFCore.Controllers
+{
+    [Route("api/languages")]
+    [ApiController]
+    public class LanguageController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public LanguageController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetAllLanguages()
+        {
+            var result = await (from lanugages in context.tbl_Languages
+                         select lanugages).ToListAsync();
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLanguageByIdAsync(int id)
+        {
+            var result = await context.tbl_Languages.FindAsync(id);
+            return Ok(result);
+        }
+    }
+}
